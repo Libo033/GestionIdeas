4af036e0f42a9805494ec9d4383ad77948a52b7f
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-10-26 14:35:16 -0300

    Main en proceso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import { Divider } from "@mui/material";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <Login forgotPass={""} signUp={""} />
+      <div className={styles.divider}>
+        <Divider/>
+      </div>
     </main>
-  )
+  );
 }
---
+++ src/components/LoginButtonGroup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginButtonGroup = () => {
+  return (
+    <div>LoginButtonGroup</div>
+  )
+}
+
+export default LoginButtonGroup
