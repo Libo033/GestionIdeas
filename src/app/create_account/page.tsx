b4b926fdbd2b9a650f0ab70aef2ea3eafa7dab5c
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-10-27 10:38:08 -0300

    Elimindas hojas de estilos vacias

--- src/app/create_account/page.tsx
+++ src/app/create_account/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./page.module.css";
 import Register from "@/components/Register";
 
 const CreateAccount = () => {
--- src/app/recover_password/page.tsx
+++ src/app/recover_password/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './page.module.css'
 import Recover from '@/components/Recover'
 
 const RecoverPassword = () => {
