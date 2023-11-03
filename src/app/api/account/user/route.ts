7de5a98c03519cb237b26260145c96444565e9ca
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-03 10:43:02 -0300

    Middleware actualizado

--- src/app/api/account/user/route.ts
+++
@@ -1,9 +0,0 @@
-export async function POST() {
-  try {
-    return Response.json({ test: "ok" }, { status: 200 });
-  } catch (error) {
-    if (error instanceof Error) {
-      return Response.json({ test: "failed" }, { status: 500 });
-    }
-  }
-}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
-import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
   const mySession: RequestCookie | undefined = request.cookies.get("mySession");
-  
+
   if (mySession !== undefined) {
     let secret_key: Uint8Array = new TextEncoder().encode("valentin");
     const value = await jwtVerify(mySession.value, secret_key);
 
-    console.log(value.payload.uid);
-
-    return NextResponse.next();
+    if (value.payload.uid) {
+      return NextResponse.next();
+    }
   }
 
   return NextResponse.redirect(new URL("/", request.url));
