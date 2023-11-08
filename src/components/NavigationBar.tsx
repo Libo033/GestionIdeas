9a44bec8af5bf147bba9d1267445cbede9646b70
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-08 10:36:32 -0300

    Creado NavigationBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Components.module.css";
+import Image from "next/image";
 
 const NavigationBar = () => {
   return (
     <nav className={styles.NavigationBar}>
       <ul className={styles.NavigationBar_ul}>
-        <li className={styles.NavigationBar_logoContainer}></li>
-        <li className={styles.NavigationBar_menuContainer}></li>
+        <li className={styles.NavigationBar_logoContainer}>
+          <Image src={"/img/logo.svg"} alt="logo" width={45} height={45} />
+          <span>IDEARIO</span>
+        </li>
+        <li className={styles.NavigationBar_menuContainer}>
+          <Image src={"/img/menu.svg"} alt="menu" width={45} height={45} />
+        </li>
       </ul>
     </nav>
   );
