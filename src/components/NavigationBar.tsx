d95353bfca350c61aae075e1f99f3c76e716d909
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-09 12:04:00 -0300

    NavigationDrawer en proceso

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./Components.module.css";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { Drawer } from "@mui/material";
+import NavigationDrawer from "./NavigationDrawer";
 
 const NavigationBar = () => {
+  const [toggleDrawer, setToggleDrawer] = useState<boolean>(false);
+  const router: AppRouterInstance = useRouter();
+
   return (
     <nav className={styles.NavigationBar}>
       <ul className={styles.NavigationBar_ul}>
-        <li className={styles.NavigationBar_logoContainer}>
+        <li
+          className={styles.NavigationBar_logoContainer}
+          onClick={() => router.push("/dashboard")}
+        >
           <Image src={"/img/logo.svg"} alt="logo" width={45} height={45} />
           <span>IDEARIO</span>
         </li>
-        <li className={styles.NavigationBar_menuContainer}>
+        <li
+          className={styles.NavigationBar_menuContainer}
+          onClick={() => setToggleDrawer(true)}
+        >
           <Image src={"/img/menu.svg"} alt="menu" width={45} height={45} />
         </li>
       </ul>
+      <Drawer
+        anchor="right"
+        onClose={() => setToggleDrawer(false)}
+        open={toggleDrawer}
+      >
+        <NavigationDrawer />
+      </Drawer>
     </nav>
   );
 };
---
+++ src/components/NavigationDrawer.tsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+import React, { useContext } from "react";
+import styles from "./Components.module.css";
+import Image from "next/image";
+import { AuthContext } from "@/context/AuthContext";
+
+const NavigationDrawer = () => {
+  const { logOut } = useContext(AuthContext);
+
+  return (
+    <div className={styles.NavigationDrawer}>
+      <div className={styles.NavigationDrawer_logoContainer}>
+        <Image src={"/img/logo.svg"} alt="think" width={72} height={72} />
+      </div>
+      <ul className={styles.NavigationDrawer_ul1}>
+        <li>
+          <p className={styles.NavigationDrawer_ulTitle}>General</p>
+        </li>
+        <li>
+          <Link className={styles.NavigationDrawer_link} href={"/dashboard"}>
+            <div className={styles.NavigationDrawer_linkContent}>
+              <Image
+                src={"/img/dashboard.svg"}
+                alt="dashboard"
+                width={45}
+                height={45}
+              />
+              <p>Dashboard</p>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.NavigationDrawer_link} href={"/dashboard"}>
+            <div className={styles.NavigationDrawer_linkContent}>
+              <Image
+                src={"/img/notes.svg"}
+                alt="notes"
+                width={45}
+                height={45}
+              />
+              <p>Notes</p>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.NavigationDrawer_link} href={"/dashboard"}>
+            <div className={styles.NavigationDrawer_linkContent}>
+              <Image
+                src={"/img/kanban.svg"}
+                alt="kanban board"
+                width={45}
+                height={45}
+              />
+              <p>Kanban</p>
+            </div>
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.NavigationDrawer_logOutContainer}>
+        {logOut && (
+          <button onClick={() => logOut()} className={styles.NavigationDrawer_logOut}>
+            <Image
+              src={"/img/logout.svg"}
+              alt="log out"
+              width={30}
+              height={30}
+            />
+            Log out
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavigationDrawer;
