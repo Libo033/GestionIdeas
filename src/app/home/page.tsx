93367cabab8419b437589f6aeb3bc07aae4587e7
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-13 16:39:14 -0300

    Creada Home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,11 +2,62 @@
 import { AuthContext } from "@/context/AuthContext";
 import React, { useContext } from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
 
   return (
+    <main className={styles.Dashboard}>
+      {user && (
+        <>
+          <div className={styles.Dashboard_welcome}>
+            <h1>Administra todas tus tareas aquí</h1>
+          </div>
+          <section className={styles.Dashboard_section}>
+            <article className={styles.Dashboard_article}>
+              <p className={styles.Dashboard_articleTitle}>KANBAN</p>
+              <Image
+                src={"/img/notesflatline.svg"}
+                alt="kanban"
+                width={210}
+                height={210}
+              />
+              <p>
+                <b>Kanban</b> es un sistema visual para gestionar el trabajo a
+                medida que avanza en un proceso. Kanban visualiza tanto el
+                proceso (el flujo de trabajo) como el trabajo real que pasa por
+                ese proceso.
+              </p>
+            </article>
+            <article className={styles.Dashboard_article}>
+              <p className={styles.Dashboard_articleTitle}>NOTES</p>
+              <Image
+                src={"/img/notesoutline.svg"}
+                alt="kanban"
+                width={210}
+                height={210}
+              />
+              <p>
+                Nuestra interfaz está diseñada para que puedas organizar tus{" "}
+                <b>notas</b> de manera intuitiva y rápida. Ya sea por proyectos,
+                temas o fechas, encontrarás la estructura que se adapte mejor a
+                tu estilo de trabajo.
+              </p>
+            </article>
+          </section>
+          <div className={styles.Dashboard_buttonAdminContainer}>
+            <button className={styles.Dashboard_buttonAdmin}>Administra tus tareas</button>
+          </div>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default Dashboard;
+
+/*
     <main className={styles.Dashboard}>
       <div className="Dashboard_Breadcrumbs">
         <span>{"/ "}</span>
@@ -52,7 +103,4 @@ const Dashboard = () => {
         </>
       )}
     </main>
-  );
-};
-
-export default Dashboard;
+*/
