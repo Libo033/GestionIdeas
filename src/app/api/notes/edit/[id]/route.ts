03aa2ade21d8a0c1bee4e315d8205f089500ac94
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-21 10:48:57 -0300

    En proceso api notes: create y edit

--- src/app/api/notes/edit/[id]/route.ts
+++ src/app/api/notes/edit/[id]/route.ts
@@ -1,3 +1,35 @@
-export async function PUT() {   // EDIT 1 NOTE
+import clientPromise from "@/libs/mongodb";
+import { Db, MongoClient, ObjectId } from "mongodb";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
 
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  // EDIT 1 NOTE
+  try {
+    const client: MongoClient = await clientPromise;
+    const mySession: RequestCookie | undefined = cookies().get("mySession");
+    const data = await req.json();
+
+    if (mySession === undefined) {
+      return Response.json({ Error: "Account doesn't exist" }, { status: 401 });
+    }
+
+    const db: Db = client.db(mySession.value);
+
+    const edit_note = {
+      title: data.title,
+      content: data.content,
+      create_date: new Date().toLocaleString(),
+      expire_date: data.expire_date,
+    };
+
+    return Response.json(edit_note, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({ Error: error.message }, { status: 500 });
+    }
+  }
 }
--- src/app/api/notes/new/route.ts
+++ src/app/api/notes/new/route.ts
@@ -1,3 +1,32 @@
-export async function POST() {   // CREATE NOTE
+import clientPromise from "@/libs/mongodb";
+import { Db, MongoClient } from "mongodb";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
 
+export async function POST(req: Request) {
+  // CREATE NOTE
+  try {
+    const client: MongoClient = await clientPromise;
+    const mySession: RequestCookie | undefined = cookies().get("mySession");
+    const data = await req.json();
+
+    if (mySession === undefined) {
+      return Response.json({ Error: "Account doesn't exist" }, { status: 401 });
+    }
+
+    const db: Db = client.db(mySession.value);
+
+    const new_note = {
+      title: data.title,
+      content: data.content,
+      create_date: new Date().toLocaleString(),
+      expire_date: data.expire_date,
+    };
+
+    return Response.json(new_note, { status: 201 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({ Error: error.message }, { status: 500 });
+    }
+  }
 }
