d4f9a3ec86f15bad65368e1a5bbda8d2b4e85a4f
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-30 14:29:28 -0300

    Creado DELETE por id en kanban

--- src/app/api/kanban/[id]/route.ts
+++ src/app/api/kanban/[id]/route.ts
@@ -55,10 +55,42 @@ export async function PUT() {
   }
 }
 
-export async function DELETE() {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   // BORRAR UN KANBAN
   try {
-    return Response.json({}, { status: 200 });
+    const client: MongoClient = await clientPromise;
+    const mySession: RequestCookie | undefined = cookies().get("mySession");
+    const id = params.id;
+    let secret_key: Uint8Array = new TextEncoder().encode(
+      process.env.JWT_SECRET
+    );
+
+    if (mySession === undefined) {
+      return Response.json({ Error: "Account doesn't exist" }, { status: 401 });
+    }
+
+    const value: JWTVerifyResult<JWTPayload> = await jwtVerify(
+      mySession.value,
+      secret_key
+    );
+
+    if (typeof value.payload.uid !== "string") {
+      throw new Error();
+    }
+
+    const db: Db = client.db(value.payload.uid);
+
+    const kanban_deleted = await db
+      .collection("notes")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    return Response.json(
+      { deleted: kanban_deleted.acknowledged },
+      { status: 200 }
+    );
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ Error: error.message }, { status: 500 });
