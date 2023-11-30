8f7622685577c4df5a28f366669c76124a1316fe
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-11-30 14:27:24 -0300

    Creado GET por id en kanban

--- src/app/api/kanban/[id]/route.ts
+++ src/app/api/kanban/[id]/route.ts
@@ -1,7 +1,42 @@
+import clientPromise from "@/libs/mongodb";
+import { Db, MongoClient, ObjectId } from "mongodb";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+import { JWTPayload, JWTVerifyResult, jwtVerify } from "jose";
 
-export async function GET() {  // OBTENER UN KANBAN
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  // OBTENER UN KANBAN
   try {
-    return Response.json({}, {status: 200});
+    const client: MongoClient = await clientPromise;
+    const id = params.id;
+    const mySession: RequestCookie | undefined = cookies().get("mySession");
+    let secret_key: Uint8Array = new TextEncoder().encode(
+      process.env.JWT_SECRET
+    );
+
+    if (mySession === undefined) {
+      return Response.json({ Error: "Account doesn't exist" }, { status: 401 });
+    }
+
+    const value: JWTVerifyResult<JWTPayload> = await jwtVerify(
+      mySession.value,
+      secret_key
+    );
+
+    if (typeof value.payload.uid !== "string") {
+      throw new Error();
+    }
+
+    const db: Db = client.db(value.payload.uid);
+    const kanban = await db
+      .collection("kanban")
+      .find({ _id: new ObjectId(id) })
+      .toArray();
+
+    return Response.json(kanban[0], { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ Error: error.message }, { status: 500 });
@@ -9,9 +44,10 @@ export async function GET() {  // OBTENER UN KANBAN
   }
 }
 
-export async function PUT() {  // EDITAR UN KANBAN
+export async function PUT() {
+  // EDITAR UN KANBAN
   try {
-    return Response.json({}, {status: 200});
+    return Response.json({}, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ Error: error.message }, { status: 500 });
@@ -19,9 +55,10 @@ export async function PUT() {  // EDITAR UN KANBAN
   }
 }
 
-export async function DELETE() {  // BORRAR UN KANBAN
+export async function DELETE() {
+  // BORRAR UN KANBAN
   try {
-    return Response.json({}, {status: 200});
+    return Response.json({}, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ Error: error.message }, { status: 500 });
