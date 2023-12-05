0c03134c313047b126311b6c00665f5d5a5aaadc
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-12-05 15:21:24 -0300

    Creado: ItemsHandler componente para crear y modificar items

--- src/components/item/ItemsHandler.tsx
+++ src/components/item/ItemsHandler.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import styles from "./Components.module.css"
-import { IItemsHandler } from '@/libs/interfaces'
+import React from "react";
+import styles from "./Components.module.css";
+import { IItemsHandler } from "@/libs/interfaces";
+import { TextField } from "@mui/material";
 
 const ItemsHandler: React.FC<IItemsHandler> = (props) => {
   return (
-    <div>ItemsHandler</div>
-  )
-}
+    <div className={styles.ItemsHandler}>
+      <form className={styles.ItemsHandler_form}>
+        <div className={styles.ItemsHandler_textField}>
+          <TextField
+            id="item_handler_text"
+            InputLabelProps={{ shrink: true }}
+            label="TEXT"
+            variant="filled"
+            fullWidth
+            required
+          />
+        </div>
+        <button type="submit" className={styles.ItemsHandler_submit}>
+          {props.idItem ? "EDIT" : "CREATE"}
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default ItemsHandler
+export default ItemsHandler;
