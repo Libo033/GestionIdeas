4a7be7936e3e59428d39824bfaaffef1108e6215
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-12-05 14:46:34 -0300

    KanbanBoardContext mejorado

--- src/context/KanbanBoardContext.tsx
+++ src/context/KanbanBoardContext.tsx
@@ -44,70 +44,91 @@ export const KanbanBoardContextProvider: React.FC<{
   }, []);
 
   const handleMoveBack = (idKanban: string, idItem: string): void => {
-    // PRIMERO MOVER EN EL USESTATE EL ITEM
-    let kanbanToMod = kanban.find((k) => k._id === idKanban);
-    let kanbanWithout = kanban.filter((k) => k._id !== idKanban);
+    try {
+      // PRIMERO MOVER EN EL USESTATE EL ITEM
+      let kanbanToMod = kanban.find((k) => k._id === idKanban);
+      let kanbanWithout = kanban.filter((k) => k._id !== idKanban);
 
-    if (kanbanToMod) {
-      kanbanToMod.content.forEach((k) => {
-        if (k._id === idItem) {
-          switch (k.status) {
-            case "doing":
-              k.status = "to do";
-              break;
-            case "done":
-              k.status = "doing";
-              break;
-            default:
-              console.log("Algo salio mal: " + k.status);
-              break;
+      if (kanbanToMod) {
+        kanbanToMod.content.forEach((k) => {
+          if (k._id === idItem) {
+            switch (k.status) {
+              case "doing":
+                k.status = "to do";
+                break;
+              case "done":
+                k.status = "doing";
+                break;
+              default:
+                throw new Error("it couldn't be moved the item");
+                break;
+            }
           }
-        }
-      });
+        });
 
-      setKanban([...kanbanWithout, kanbanToMod]);
+        setKanban([...kanbanWithout, kanbanToMod]);
+      }
+      // SEGUNDO MOVERLO DE LA DB CON LA API
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
     }
-    // SEGUNDO MOVERLO DE LA DB CON LA API
   };
 
   const handleMoveNext = (idKanban: string, idItem: string): void => {
-    // PRIMERO MOVER EN EL USESTATE EL ITEM
-    let kanbanToMod = kanban.find((k) => k._id === idKanban);
-    let kanbanWithout = kanban.filter((k) => k._id !== idKanban);
+    try {
+      // PRIMERO MOVER EN EL USESTATE EL ITEM
+      let kanbanToMod = kanban.find((k) => k._id === idKanban);
+      let kanbanWithout = kanban.filter((k) => k._id !== idKanban);
 
-    if (kanbanToMod) {
-      kanbanToMod.content.forEach((k) => {
-        if (k._id === idItem) {
-          switch (k.status) {
-            case "to do":
-              k.status = "doing";
-              break;
-            case "doing":
-              k.status = "done";
-              break;
-            default:
-              console.log("Algo salio mal: " + k.status);
-              break;
+      if (kanbanToMod) {
+        kanbanToMod.content.forEach((k) => {
+          if (k._id === idItem) {
+            switch (k.status) {
+              case "to do":
+                k.status = "doing";
+                break;
+              case "doing":
+                k.status = "done";
+                break;
+              default:
+                throw new Error("it couldn't be moved the item");
+                break;
+            }
           }
-        }
-      });
+        });
 
-      setKanban([...kanbanWithout, kanbanToMod]);
+        setKanban([...kanbanWithout, kanbanToMod]);
+      }
+      // SEGUNDO MOVERLO DE LA DB CON LA API
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
     }
-    // SEGUNDO MOVERLO DE LA DB CON LA API
   };
 
   const handleDeleteItem = (idKanban: string, idItem: string) => {
-    // PRIMERO ELIMINAR DEL USESTATE EL ITEM
-    let kanbanToMod = kanban.find((k) => k._id === idKanban);
-    let kanbanWithout = kanban.filter((k) => k._id !== idKanban);
+    try {
+      // PRIMERO ELIMINAR DEL USESTATE EL ITEM
+      let kanbanToMod = kanban.find((k) => k._id === idKanban);
+      let kanbanWithout = kanban.filter((k) => k._id !== idKanban);
 
-    if (kanbanToMod) {
-      kanbanToMod.content = kanbanToMod.content.filter((k) => k._id !== idItem);
+      if (kanbanToMod) {
+        // delete item by ID
+        kanbanToMod.content = kanbanToMod.content.filter(
+          (k) => k._id !== idItem
+        );
 
-      setKanban([...kanbanWithout, kanbanToMod]);
+        setKanban([...kanbanWithout, kanbanToMod]);
+      }
+      // SEGUNDO ELIMINARLO DE LA DB CON LA API
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
     }
-    // SEGUNDO ELIMINARLO DE LA DB CON LA API
   };
 
   return (
