c72657b83e877ca37ada37c945ed07a0c1e768a3
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-12-14 12:29:11 -0300

    Creado: dashboard en proceso

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -3,9 +3,18 @@ import { AuthContext } from "@/context/AuthContext";
 import Link from "next/link";
 import React, { useContext } from "react";
 import styles from "./page.module.css";
+import Note from "@/components/notes/Note";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
+
+  const noteData = {
+    _id: "1",
+    title: "valentinlibonati33@gmail.com",
+    content: "12346789 asd",
+    create_date: "22/11/2023, 14:41:49",
+    expire_date: 1
+  }
 
   return (
     <main className={styles.Dashboard}>
@@ -15,7 +24,21 @@ const Dashboard = () => {
         <span>{" / "}</span>
         <span>Tablero</span>
       </div>
-      Tablero
+      <h1>Tablero</h1>
+      <div className={styles.Dashboard_container}>
+        <div className={styles.Dashboard_notes}>
+          <p className={styles.Dashboard_notesSubTitle}>Proximas notas:</p>
+          <section>
+            <Note {...noteData} />
+            <Note {...noteData} />
+            <Note {...noteData} />
+            <Note {...noteData} />
+          </section>
+        </div>
+        <div className={styles.Dashboard_kanban}>
+          <p className={styles.Dashboard_kanbanSubTitle}>Kanban en proceso:</p>
+        </div>
+      </div>
     </main>
   );
 };
